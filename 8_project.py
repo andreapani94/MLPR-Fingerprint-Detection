a2a59762ae00057d80c696d1b8223cc441b686a2
ANDREA PANI s305783 <s305783@studenti.polito.it>
2024-07-10 14:17:07 +0200

    Calibration part added

--- 8_project.py
+++ 8_project.py
@@ -1,5 +1,5 @@
-from libs.utils import load_data, split_2to1, to_snake_case
-from libs.gaussian_mixture_models import GMM
+from libs.utils import load_data, split_2to1, to_snake_case, print_table
+from libs.gaussian_mixture_models import GMM, save_gmm
 from libs.model_evaluation import bayes_pred_llr, confusion_matrix, DCF, DCF_min
 import numpy as np
 import matplotlib.pyplot as plt
@@ -18,10 +18,14 @@ def plot_gmm_dcfs(G_values: list[int], dcfs: list[list, list], title: str, savep
     plt.close()
 
 pi_true = 0.1
+filepath = 'results/gaussian_mixture_models.txt'
  
 def main():
     data, labels = load_data('dataset/train.txt')
     (train_data, train_labels), (val_data, val_labels) = split_2to1(data, labels)
+    #   clear results file
+    with open(filepath, 'w') as f:
+        f.truncate()
     G_values = 2**np.arange(0, 6)
     for title, covtype in [
         ('Full Covariance', 'full'), 
@@ -29,12 +33,15 @@ def main():
         ('Tied Covariance', 'tied')
     ]:
         dcfs = [[], []]
+        table_rows = []
+        models = []
         for G in G_values:
             model = GMM(n_components=G, covtype=covtype)
             scores = []
             for label in [0, 1]:
                 model.train(train_data[:, train_labels==label], conv_threshold=1e-6, covbound=0.01)
                 scores.append(model(val_data))
+                models.append(model)
             scores = np.vstack(scores)
             llr_scores = scores[1] - scores[0]
             predictions = bayes_pred_llr(llr_scores, pi_true)
@@ -43,12 +50,12 @@ def main():
             dcfs[0].append(actDCF)
             minDCF = DCF_min(llr_scores, val_labels, pi_true)
             dcfs[1].append(minDCF)
-            np.save(f'results/gaussian_mixture_models/GMM(G={G})_{covtype}_llrs', llr_scores)  
+            table_rows.append((G, actDCF, minDCF, f'{((actDCF - minDCF) / minDCF) * 100:.2f}%'))
+            np.save(f'results/gmm/GMM(G={G})_{covtype}_llrs', llr_scores) 
+            for gmm in models:
+                save_gmm(gmm.params, f'models/gmm/GMM(G={G})_{covtype}_params')
         plot_gmm_dcfs(G_values, dcfs, f'GMM {title}', f'plots/GMM_{covtype}_G_DCFs_plot')
-        # np.save(
-        #     f'models/gaussian_models/GMM(G={G})_{covtype}_params',
-
-        # )
+        print_table(table_rows, ['G', 'actDCF', 'minDCF', 'Calibration loss'], filepath=filepath, title=f'GMM {title}')
     
 
 if __name__ == '__main__':
--- 9_project.py
+++ 9_project.py
@@ -1,102 +1,61 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from libs.utils import load_data, split_2to1, col, row
-from support_vector_machines import SVM, poly_kernel, rbf_kernel
-from model_evaluation import DCF, DCF_min, bayes_pred_llr, confusion_matrix
+from libs.logistic_regression import LogisticRegression
+from libs.utils import load_data, split_2to1, row, col
+from libs.model_calibration import Kfold_split
+from libs.model_evaluation import bayes_pred_llr, confusion_matrix, DCF, DCF_min
 
-def plot_svm_dcfs(C_values: list[float], dcfs: list[list, list], title: str):
-    plt.figure()
-    plt.xscale('log', base=10)
-    plt.plot(C_values, dcfs[0], label='actDCF')
-    plt.plot(C_values, dcfs[1], linestyle='--', label='minDCF')
-    plt.xlabel('C (regularization parameter)')
-    plt.ylabel('DCFs')
-    plt.ylim(bottom=0)
-    plt.legend()
-    plt.savefig(f'./plots/{title}')
-    plt.close()
+best_scores_filenames = [
+    'results/logistic_regression/quadratic_logreg(λ=3.1623e-02)_llrs.npy',
+    'results/gmm/GMM(G=8)_diag_llrs.npy'
+]
 
-def plot_kernsvm_dcfs(C_values: list[float], param_dcfs: dict[str, list[list, list]], title: str):
-    plt.figure()
-    plt.xscale('log', base=10)
-    for param in param_dcfs:
-        plt.plot(C_values, param_dcfs[param][0], label=f'actDCF ({param})')
-        plt.plot(C_values, param_dcfs[param][1], linestyle='--', label=f'minDCF ({param})')
-    plt.xlabel('C (regularization parameter)')
-    plt.ylabel('DCFs')
-    plt.ylim(bottom=0)
-    plt.legend()
-    plt.savefig(f'./plots/{title}')
-    plt.close()
-
-pi = 0.1
-
+pi_true = 0.1
+K = 5
+   
 def main():
-    D, l = load_data('dataset/train.txt')
-    (Dtr, ltr), (Dval, lval) = split_2to1(D, l)
-    C_values = np.logspace(-5, 0, 11)
-    Dtr, ltr = Dtr[:, ::100], ltr[::100] # just for experiments
-    # LINEAR SVM
-    K = 1
-    mu = col(np.mean(Dtr, 1))
-    for Dtr, ltr, Dval, title in [(Dtr, ltr, Dval, ''), (Dtr - mu, ltr, Dval - mu, '_centered')]:
-        dcfs = [[], []]
-        for C in C_values:
-            model = SVM(C, K)
-            model.train(Dtr, ltr)
-            S = model(Dval)
-            lpred = np.where(S > 0, 1, 0).ravel()
-            err = 1 - (np.sum(lpred == lval) / len(lval))
-            M = confusion_matrix(lpred, lval)
-            actDCF, minDCF = DCF(M, pi), DCF_min(S, lval, pi)
-            dcfs[0].append(actDCF)
-            dcfs[1].append(minDCF)
-            # save model parameters and scores
-            np.save(f'./models/svm_C={C}', np.hstack([model.params[0].ravel(), model.params[1].ravel()]))
-            np.save(f'./results/svm_C={C}_scores', S)
-        plot_svm_dcfs(C_values, dcfs, f'svm_C_DCFs{title}')
+    data, labels = load_data('dataset/train.txt')
+    _, (_, val_labels) = split_2to1(data, labels)
+    calibrated_scores = []
+    # load the scores of the best performing models
+    for score_name in best_scores_filenames:
+        scores = row(np.load(score_name))
+        model_cal = LogisticRegression(prior=pi_true)
+        Kfold_scores, Kfold_labels = list(), list()
+        for (train_cal_data, train_cal_labels), (val_cal_data, val_cal_labels) in \
+            Kfold_split(scores, val_labels, K=K, shuffle=True):
+                model_cal.train(train_cal_data, train_cal_labels)
+                llr_scores_cal = model_cal(val_cal_data) - np.log(pi_true / (1 - pi_true))
+                Kfold_scores.append(llr_scores_cal)
+                Kfold_labels.append(val_cal_labels)
+        # pool the scores and labels
+        Kfold_scores, Kfold_labels = np.hstack(Kfold_scores), np.hstack(Kfold_labels)
+        # compute actDCF and minDCF of the pooled calibrated scores
+        predictions = bayes_pred_llr(Kfold_scores, pi_true)
+        confmatrix = confusion_matrix(predictions, Kfold_labels)
+        actDCF = DCF(confmatrix, pi_true)
+        minDCF = DCF_min(Kfold_scores, Kfold_labels, pi_true)
+        pass
+    effprior_logodds = np.linspace(-4, 4, 21)
+    effpriors = 1 / (1 + np.exp(-effprior_logodds))
+    model_dcf_map = {}
+    for model_name, llr_scores in model_llr_scores:
+        actDCFs, minDCFs = list(), list()
+        for prior_eff in effpriors:
+            predictions = bayes_pred_llr(llr_scores, prior_eff)
+            M = confusion_matrix(predictions, val_labels)
+            dcf_score = DCF(M, prior_eff)
+            actDCFs.append(dcf_score)
+            mindcf_score = DCF_min(llr_scores, val_labels, prior_eff)
+            minDCFs.append(mindcf_score)
+        model_dcf_map[model_name] = {'actDCF': actDCFs, 'minDCF': minDCFs}
+    plot_bayer_error(
+        prior_logodds=effprior_logodds,
+        model_dcf_map=model_dcf_map,
+        savepath='plots/gaussian_models_DCF_comparison',
+        title='MVG Variants',
+        separate_plots=False
+    )
 
-    # KERNEL SVM
-    #   polynomial kernel
-    d, c = 2, 1
-    K = 0
-    dcfs = [[], []]
-    for C in C_values:
-            model = SVM(C, K, poly_kernel(d, c))
-            model.train(Dtr, ltr)
-            S = model(Dval)
-            lpred = np.where(S > 0, 1, 0).ravel()
-            err = 1 - (np.sum(lpred == lval) / len(lval))
-            M = confusion_matrix(lpred, lval)
-            actDCF, minDCF = DCF(M, pi), DCF_min(S, lval, pi)
-            dcfs[0].append(actDCF)
-            dcfs[1].append(minDCF)
-            # save model parameters and scores
-            np.save(f'./models/svm_C={C}_poly(d={c},c={c})', np.hstack([model.params[0], model.params[1]]))
-            np.save(f'./results/svm_C={C}_poly(d={c},c={c})_scores', S)
-    plot_kernsvm_dcfs(C_values, {f'd={c},c={c}': dcfs}, f'kernsvm_C_DCFs_poly')
-    #   RBF kernel
-    C = np.logspace(-3, 2, 11)
-    g_values = np.exp(-np.arange(1, 5, -1))
-    K = 1
-    plot_dict = {}
-    for g in g_values:
-        dcfs = [[], []]
-        for C in C_values:
-            model = SVM(C, K, rbf_kernel(g))
-            model.train(Dtr, ltr)
-            S = model(Dval)
-            lpred = np.where(S > 0, 1, 0).ravel()
-            err = 1 - (np.sum(lpred == lval) / len(lval))
-            M = confusion_matrix(lpred, lval)
-            actDCF, minDCF = DCF(M, pi), DCF_min(S, lval, pi)
-            dcfs[0].append(actDCF)
-            dcfs[1].append(minDCF)
-            plot_dict[f'γ={g}'] = dcfs
-            # save model parameters and scores
-            np.save(f'./models/svm_C={C}_RBF(γ={g}))', np.hstack([model.params[0], model.params[1]]))
-            np.save(f'./results/svm_C={C}_RBF(γ={g}))_scores', S)
-    plot_kernsvm_dcfs(C_values, plot_dict, f'kernsvm_C_DCFs_RBF')    
 
 
 if __name__ == '__main__':
